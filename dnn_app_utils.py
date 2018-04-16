ba7f7f8cec2a4492c86419c4f4952de3d629acea
Buddha Shrestha <shrestbuddha@gmail.com>
2018-04-15 23:55:55 -0500

    Tuning params

--- dnn_app_utils.py
+++ dnn_app_utils.py
@@ -147,17 +147,15 @@ def initialize_parameters_deep(layer_dims):
                     bl -- bias vector of shape (layer_dims[l], 1)
     """
     
-    np.random.seed(1)
+    np.random.seed(3)
     parameters = {}
-    L = len(layer_dims)            # number of layers in the network
-
-    for l in range(1, L):
-        parameters['W' + str(l)] = np.random.randn(layer_dims[l], layer_dims[l-1]) * 0.1 / np.sqrt(layer_dims[l-1]) 
-        parameters['b' + str(l)] = np.zeros((layer_dims[l], 1))
-        
-        assert(parameters['W' + str(l)].shape == (layer_dims[l], layer_dims[l-1]))
-        assert(parameters['b' + str(l)].shape == (layer_dims[l], 1))
-
+    L = len(layers_dims) - 1 # integer representing the number of layers
+     
+    for l in range(1, L + 1):
+        ### START CODE HERE ### (≈ 2 lines of code)
+        parameters['W' + str(l)] = np.random.randn(layers_dims[l], layers_dims[l - 1]) * np.sqrt(2 / layers_dims[l - 1])
+        parameters['b' + str(l)] = np.zeros((layers_dims[l], 1))
+        ### END CODE HERE ###
         
     return parameters
 
